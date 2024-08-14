a7685506b9b97a0a89b7e4beb96be606eefa642f
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-08-14 12:52:22 -0600

    Added put route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,8 +7,8 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const TagsData = await Tag.findAll({ include: [{ model: Product }] });
-    res.status(200).json(TagsData);
+    const tagsData = await Tag.findAll({ include: [{ model: Product }] });
+    res.status(200).json(tagsData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -18,16 +18,16 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const TagData = await Tag.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
 
-    if (!TagData) {
+    if (!tagData) {
       res.status(404).json({ message: 'No Tag found with this id!' });
       return;
     };
 
-    res.status(200).json(TagData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -36,16 +36,28 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const TagData = await Tag.create(req.body);
-    res.status(200).json(TagData);
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update({ tag_name: req.body.tag_name }, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
