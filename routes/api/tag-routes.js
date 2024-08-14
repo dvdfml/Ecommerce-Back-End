af0eaf9cc06da609ccdea87ad1e85b27e241c2f2
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-08-14 12:44:39 -0600

    Added post route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,8 +33,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const TagData = await Tag.create(req.body);
+    res.status(200).json(TagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 router.put('/:id', (req, res) => {
