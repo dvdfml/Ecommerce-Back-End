dd24546cc5614c09850ceab9ca375d2ebeb748d1
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-08-19 12:28:00 -0600

    Fixed repeated fields in query results

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagsData = await Tag.findAll({ include: [{ model: Product }] });
+    const tagsData = await Tag.findAll({ include: [{ model: Product, through: { attributes: ['id', 'product_id', 'tag_id'] } }] });
     res.status(200).json(tagsData);
   } catch (err) {
     res.status(500).json(err);
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }]
+      include: [{ model: Product, through: { attributes: ['id', 'product_id', 'tag_id'] } }]
     });
 
     if (!tagData) {
